a11f02608cf877cf252814718faea8de9bd6e69d
josecleiton <jcleitonbc@gmail.com>
2018-06-18 18:20:47 -0300

    resolução de candido

--- ba18/h.cpp
+++ ba18/h.cpp
@@ -1,34 +1,37 @@
 #include <bits/stdc++.h>
-#define FOR(a) for(int i=0;i<a;i++)
-#define C(a) cin>>a
-bool compare(int& x, int&y){return x>y;}
+#define FOR(x) for(int i=0;i<x;i++)
+#define FORJ(x) for(int j=0;j<x;j++)
+#define FORQ(Q) for(int i=0;Q;i++)
+#define FORM(x,y) for(int i=0;i<x;i++) for(int j=0;j<y;j++)
+#define FORT(x,y) for(int i=x;i<y;i++)
+#define WHILE(n,x) while((n--)&&cin>>x)
+#define M(x,y) max(x, y)
+#define m(x,y) min(x, y)
+#define c(x) cout<<x<<endl
+
 using namespace std;
 
-int main(){
-int n,a,b,m,s,ax,t;
-vector<int> v;
-priority_queue<int> p;
-cin>>a>>b;
-v.resize(a);
-FOR(a){
-	cin>>m;
-	p.push(m);
+int main()
+{
+    long long int n, q, b=0, ax=0, s=0;
+    vector<int> a;
+    cin>>n>>q;
+    a.resize(n);
+    FOR(n){
+        cin>>a[i];  
+    }
+    sort(a.begin(), a.end());
+    for(int i=1; i<n; i++){
+        a[i] += a[i-1];
+    }
+    FOR(q){
+        cin>>ax;
+        auto it=lower_bound(a.begin(), a.end(), ax);
+        int index=it-a.begin();
+        //int index=distance(a.begin(),it);
+        if(a[index]>ax) c(index);
+        else c(index+1);
+    }
+    return 0;
 }
-for(int i=a-1;i>=0;i--){ 
-	v[i] = p.top();
-	p.pop();
-	
-}
-FOR(b){
-	cin>>m;
-	s=0;
-	t=0;
-	for(int j=0;j<a && t<m;j++){
-		t += v[j];
-		s++;	
-	}
-	if(t>m) cout<<s-1<<endl;
-	else cout<<s<<endl;
-}
-return 0;
-}
+
