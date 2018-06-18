70e04866d23dede8d0e719cc974a373fe7a33812
josecleiton <jcleitonbc@gmail.com>
2018-06-18 18:20:36 -0300

    mais macros

--- template.cpp
+++ template.cpp
@@ -1,12 +1,23 @@
+//TEMPLATE COM MACROS PARA OS CONTESTS
+//compile usando: g++ -std=c++11 -lm -O3 arquivo.cpp
 #include <bits/stdc++.h>
-#define FOR(a,b) for(int i=a; i<b; i++)
-#define FOR2(a,b) for(int i=a-1; i>=b; i--)
-#define C(a) cin>>a
+#define FOR(x) for(int i=0;i<x;i++)
+#define ROF(x) for(int i=x-1;i>=0;i--)
+#define FORJ(x) for(int j=0;j<x;j++)
+#define FORQ(Q) for(int i=0;Q;i++)
+#define FORM(x,y) for(int i=0;i<x;i++) for(int j=0;j<y;j++)
+#define FORT(x,y) for(int i=x;i<y;i++)
+#define ROFT(x,y) for(int i=x-1;i>=y;i--)
+#define WHILE(n,x) while((n--)&&cin>>x)
+#define M(x,y) max(x, y)
+#define m(x,y) min(x, y)
+#define c(x) cout<<x<<endl
+#define C(x) cin>>x
+
 using namespace std;
+
 int main(){
-
 int n,m,p,q,x,y,a,b,c,d,e,f;
-
 
 return 0;
 }
