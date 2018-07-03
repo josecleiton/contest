dfb34a14a7cda15bee948e3ca0787627dec7fa34
josecleiton <jcleitonbc@gmail.com>
2018-07-03 17:20:48 -0300

    dia 2

---
+++ uri/iniciante/1160.cpp
@@ -0,0 +1,40 @@
+//TEMPLATE COM MACROS PARA OS CONTESTS
+//compile usando: g++ -std=c++11 -lm -O3 arquivo.cpp
+#include <bits/stdc++.h>
+#define FOR(x) for(int i=0;i<x;i++)
+#define ROF(x) for(int i=x-1;i>=0;i--)
+#define FORJ(x) for(int j=0;j<x;j++)
+#define FORQ(Q) for(int i=0;Q;i++)
+#define FORM(x,y) for(int i=0;i<x;i++) for(int j=0;j<y;j++)
+#define FORT(x,y) for(int i=x;i<y;i++)
+#define ROFT(x,y) for(int i=x-1;i>=y;i--)
+#define WHILE(n,x) while((n--)&&cin>>x)
+#define M(x,y) max(x, y)
+#define m(x,y) min(x, y)
+#define c(x) cout<<x<<endl
+#define C(x) cin>>x
+#define set(a,b) cout.precision(a); cout<<fixed<<b<<endl
+#define pcs(a) cout.precision(a)
+#define fx(a) fixed<<a
+#define gl(s) getline(cin,s)
+#define pb(a) push_back(a)
+
+using namespace std;
+
+int main(){
+int t,pa,pb, k;
+double g1,g2;
+C(t);
+FOR(t){
+    cin>>pa>>pb>>g1>>g2;
+    k=0;
+    while(pa<=pb && k<=100){
+        pa+=pa*(g1/(double)100);
+        pb+=pb*(g2/(double)100);
+        k++;
+    }
+    if(k<=100) cout<<k<<" anos."<<endl;
+    else cout<<"Mais de 1 seculo."<<endl;
+}
+return 0;
+}
