e9d36ee767b80f1f25833532577a987bfc039a07
José Cleiton Correia <jcleitonbc@gmail.com>
2021-09-12 07:44:57 -0400

    uri 1890 e 1891

---
+++ uri/usp2015/1890.cpp
@@ -0,0 +1,74 @@
+// TEMPLATE COM MACROS PARA OS CONTESTS
+// compile usando: g++ -std=c++11 -lm -O3 arquivo.cpp
+#include <bits/stdc++.h>
+#define SET(a, b)                                                              \
+  cout.precision(a);                                                           \
+  cout << fixed << b << endl
+#define PCS(a) cout.precision(a)
+#define FX(a) fixed << a
+#define FOR(i, a) for (int i = 0; i < a; i++)
+#define PARA(i, k, n) for (int i = k; i < n; i++)
+#define ROF(i, a) for (int i = a - 1; i >= 0; i--)
+#define FORIT(it, c) for (auto it = c.begin(); it != c.end(); it++)
+#define pb emplace_back
+#define pf emplace_front
+#define pob pop_back
+#define pof pop_front
+#define MAP(t1, t2, ord) map<t1, t2, ord<t1>>
+#define UMAP(t1, t2) unordered_map<t1, t2>
+#define MATRIX(name, type, n, m) vector<vector<type>> name(n, vector<type>(m))
+#define PQ(type, ord) priority_queue<type, deque<type>, ord<type>>
+#define mkp(a, b) make_pair(a, b)
+#define TAM
+
+using namespace std;
+typedef vector<int> vi;
+typedef int64_t ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<ll> vll;
+typedef vector<bool> vbit;
+typedef vector<string> vstr;
+typedef vector<double> vd;
+typedef unordered_map<int, unordered_map<int, bool>> _graph;
+typedef unordered_map<int, unordered_map<int, int>> _wgraph;
+
+/*
+class Comp {
+public:
+    bool operator() (const type& a, const type& b) { // COMPARADOR ESTRITO PARA
+PRIORITY_Q, UMAP ETC return true;
+    }
+}
+
+MODULAR ARITHMETIC
+( a + b) % c = ( ( a % c ) + ( b % c ) ) % c
+( a * b) % c = ( ( a % c ) * ( b % c ) ) % c
+( a – b) % c = ( ( a % c ) – ( b % c ) ) % c
+
+*/
+
+int main() {
+  // ios::sync_with_stdio(0);
+  // cin.tie(0);
+  int ct;
+  // char c;
+  // ll n, m, l;
+  int n, m, l;
+  unsigned long long x, y;
+  // string s;
+
+  while (scanf("%d", &ct) != EOF) {
+    FOR(i, ct) {
+      scanf("%d%d", &n, &m);
+      if (!n && !m) {
+        puts("0");
+        continue;
+      }
+      printf("%llu\n", (unsigned long long)(pow(26, n) * pow(10, m)));
+    }
+  }
+  return 0;
+}
