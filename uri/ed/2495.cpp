94468b64edd3bb1a62c11dff6b89cd72a1ecdfb8
José Cleiton Correia <jcleitonbc@gmail.com>
2021-09-12 07:44:36 -0400

    uri 2495

--- uri/ed/2495.cpp
+++ uri/ed/2495.cpp
@@ -1,27 +1,29 @@
-//TEMPLATE COM MACROS PARA OS CONTESTS
-//compile usando: g++ -std=c++11 -lm -O3 arquivo.cpp
+// TEMPLATE COM MACROS PARA OS CONTESTS
+// compile usando: g++ -std=c++11 -lm -O3 arquivo.cpp
 #include <bits/stdc++.h>
-#define FOR(x) for(int i=0;i<x;i++)
-#define ROF(x) for(int i=x-1;i>=0;i--)
-#define ROFJ(x) for(int j=x-1;j>=0;j--)
-#define FORJ(x) for(int j=0;j<x;j++)
-#define FORQ(Q) for(int i=0;Q;i++)
-#define FORM(x,y) for(int i=0;i<x;i++) for(int j=0;j<y;j++)
-#define FORT(x,y) for(int i=x;i<y;i++)
-#define ROFT(x,y) for(int i=x-1;i>=y;i--)
-#define WHILE(n,x) while((n--)&&cin>>x)
+#define FOR(x) for (int i = 0; i < x; i++)
+#define ROF(x) for (int i = x - 1; i >= 0; i--)
+#define ROFJ(x) for (int j = x - 1; j >= 0; j--)
+#define FORJ(x) for (int j = 0; j < x; j++)
+#define FORQ(Q) for (int i = 0; Q; i++)
+#define FORM(x, y)                                                             \
+  for (int i = 0; i < x; i++)                                                  \
+    for (int j = 0; j < y; j++)
+#define FORT(x, y) for (int i = x; i < y; i++)
+#define ROFT(x, y) for (int i = x - 1; i >= y; i--)
+#define WHILE(n, x) while ((n--) && cin >> x)
 #define SORT(v) sort(v.begin(), v.end())
 #define SORTC(v, comp) sort(v.begin(), v.end(), comp)
-#define FORIT(v) for(auto& it: v)
-#define c(x) cout<<x<<endl
-#define C(x) cin>>x
-#define set(a,b) cout.precision(a); cout<<fixed<<b<<endl
+#define c(x) cout << x << endl
+#define C(x) cin >> x
 #define pcs(a) cout.precision(a)
-#define fx(a) fixed<<a
-#define gl(s) getline(cin,s)
+#define fx(a) fixed << a
+#define gl(s) getline(cin, s)
 #define pb(a) push_back(a)
-#define matrixM(n,m) vector<vector<int>> M (n, vector<int> (m))
-#define matrixN(n,m) vector<vector<int>> N (n, vector<int> (m))
+#define matrixM(n, m) vector<vector<int>> M(n, vector<int>(m))
+#define matrixN(n, m) vector<vector<int>> N(n, vector<int>(m))
+#define TAM 100005
+#define FORIT(it, c) for (auto it = c.begin(); it != c.end(); it++)
 
 using namespace std;
 typedef long long ll;
@@ -37,18 +39,22 @@ typedef map<char, int> mci;
 typedef map<string, int> msi;
 typedef pair<int, int> pii;
 
-int main(){
-    int n,m,l;
-    vector<bool> w(100005);
-    vector<bool>::iterator s;
-    while(cin>>n){
-        FOR(n-1){
-            cin>>m;
-            w[m-1]=true;
-        }
-        w[n-1]=false;
-        s=find(w.begin(), w.begin()+n, false);
-        cout<<s-w.begin()+1<<endl;
+int main() {
+  int n, m, l;
+  bitset<TAM> w;
+
+  while (scanf("%d", &n) != EOF) {
+    w.reset();
+    FOR(n - 1) {
+      scanf("%d", &m);
+      w.set(m - 1, true);
     }
-    return 0;
+    FOR(n) {
+      if (!w[i]) {
+        printf("%d\n", i + 1);
+        break;
+      }
+    }
+  }
+  return 0;
 }
